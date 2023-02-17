ac972a3b448cf7de3919dec203bfe078f8053d85
TeoSigno <volamatteo@gmail.com>
2023-02-17 17:53:10 +0100

    test di modifica da json

---
+++ test.js
@@ -0,0 +1,16 @@
+const assert = require("assert")  //require importa un modulo di Node (al posto di import)
+//cercare se possibile installare assert via NPM
+
+function convertitore(p1, p2, p3){
+    return 0;
+
+}
+
+//insieme di test
+describe("test fixture", () => {
+
+    it("testName1", () => {
+        assert.equal(1, 1)
+    })
+
+} );   
